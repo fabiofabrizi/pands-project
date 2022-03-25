a47ec87e7bfb6f1546194964c0c8b53cdbcc118f
unknown <fabio.fabrizi@gmail.com>
2022-03-25 13:50:32 +0000

    Updated converter2.py

--- converter2.py
+++ converter2.py
@@ -1,7 +1,7 @@
 # The purpose of this file is to convert the .data files to csv format so that they can be 
 # worked on. 
 # source: https://stackoverflow.com/questions/31797013/how-to-open-a-data-file-extension
-#
+# source: https://datascience.stackexchange.com/questions/97633/convert-data-file-to-csv
 # Author: Fabio Fabrizi
 
 # Import pandas so that we can convert file
@@ -11,7 +11,6 @@ import pandas as pd
 # Read the data file from the website itself
 # NB The headers (attributes) are specified on the web page itself
 # https://archive.ics.uci.edu/ml/datasets/Iris
-#iris = pd.read_csv('iris.data', header=None, names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 
 iris = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None,names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 # This is where the conversion happens
