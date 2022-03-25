e354b847f043ed11abd2f17266e645fc896ec775
unknown <fabio.fabrizi@gmail.com>
2022-03-25 12:30:34 +0000

    Upload of original data file and conversion to csv to work with. Also included is script that was used to convert between data and csv.

---
+++ converter2.py
@@ -0,0 +1,21 @@
+# The purpose of this file is to convert the .data files to csv format so that they can be 
+# worked on. 
+# source: https://stackoverflow.com/questions/31797013/how-to-open-a-data-file-extension
+#
+# Author: Fabio Fabrizi
+
+# Import pandas so that we can convert file
+import pandas as pd
+
+
+# Read the data file from the website itself
+# NB The headers (attributes) are specified on the web page itself
+# https://archive.ics.uci.edu/ml/datasets/Iris
+#iris = pd.read_csv('iris.data', header=None, names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+
+iris = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None,names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
+# This is where the conversion happens
+#iris.to_csv('iris2.csv', index = None)
+
+iris.to_csv('pands-project/iris2.csv', index = None)
+#type(iris)
