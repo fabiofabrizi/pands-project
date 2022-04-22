29074c9a33780a1971dec8e11bcb32207d9a6154
unknown <fabio.fabrizi@gmail.com>
2022-04-22 14:31:21 +0100

    Added function call to analysis.py for species count

--- analysis.py
+++ analysis.py
@@ -7,7 +7,7 @@
 # import from the other files
 from converter2 import *
 from EDA import *
-
+from dataVis import *
 
 # Convert the data first
 convert_data()
@@ -17,3 +17,7 @@ exploratory()
 
 # Print Preliminary results to text file - see analysis.txt in folder
 eda_to_text()
+
+# Plot of species count - This is just a visual representation of 
+# df.value_counts("species")
+species()
