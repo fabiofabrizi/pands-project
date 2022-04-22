e993212a1eb5b6ec2570d5b5f465d737f6c42f96
unknown <fabio.fabrizi@gmail.com>
2022-04-22 14:11:20 +0100

    Added dataVis.py for the visualisation

---
+++ dataVis.py
@@ -0,0 +1,18 @@
+# This is the Data Visualisation part of the project
+# 
+# Fabio Fabrizi
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy as sp
+
+
+# From EDA.py, we know how many species there are: 3
+# Lets see it visually, counting the number of each
+df = pd.read_csv("iris2.csv")
+
+sns.countplot(x='species', data=df, )
+plt.show()
