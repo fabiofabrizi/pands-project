d57d001814216815fc20dc458217c8e42a935e6a
unknown <fabio.fabrizi@gmail.com>
2022-04-23 17:34:52 +0100

    Updated analysis.py with the function calls for the plots

--- analysis.py
+++ analysis.py
@@ -20,10 +20,15 @@ eda_to_text()
 
 # Plot of species count - This is just a visual representation of 
 # df.value_counts("species")
-#species()
+species()
 
 # Examination of sepal length and width of the three species
-
-species()
 petal_relation() 
 sepal_relation()
+petal_sepal_length_relation()
+petal_sepal_width_relation()
+
+# This is pair plot of the sepal length/width and petal length/width
+# If you only had to look at one plot, look at this one as you can see the correlation 
+# between the petal length and petal width.
+pair_plots()
